09afae51e2ef02fd31c8be587ed0791287fa1a7a
Ulad Birukou <alorine99@gmail.com>
2020-09-18 09:23:14 +0300

    addDateTimerDayof2

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -7,21 +7,42 @@ function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse( new Date() );
     const day = Math.floor( t / (1000*60*60*24));
     const hours = Math.floor( t / (1000*60*60) % 24);
-    const minuts = Math.floor( t / 1000 / 60 % 60 );
+    const minutes = Math.floor( t / 1000 / 60 % 60 );
     const seconds = Math.floor( t / 1000 % 60 );
     
     return { 
         'total': t,
         'days': day,
         'hours': hours,
-        'minuts': minuts,
+        'minutes': minutes,
         'seconds': seconds
     };
 }
 
 function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval( updateClock, 1000);
 
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+        
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+
+    if(t.total <= 0) {
+        clearInterval(timeInterval);
+    }
+
+    }  
 }
+
+setClock('.timer', deadline);
 
 
 // const date =  new Date();
